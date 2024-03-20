8d08153daa2b83b13f85799056fe1b980c417351
ago19 <ago19pc@gmail.com>
2024-03-20 20:53:26 +0100

    Connection: ClientHandler

--- src/main/java/Connections/ClientHandler.java
+++ src/main/java/Connections/ClientHandler.java
@@ -2,6 +2,7 @@ package main.java.Connections;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -11,6 +12,8 @@ public class ClientHandler extends Thread {
     PrintWriter out;
     public ClientHandler(Socket client){
         this.socket = client;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+        out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
     }
 
     public void run() {
