624be51383bee2d3664e3bc3f954c2e8f21f7fcb
federicoconsorte <federico.consorte@mail.polimi.it>
2024-04-06 19:24:40 +0200

    added out to gitignore, removed unnecessary libraries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,6 @@ module it.am04.demo1 {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires java.sql;
     requires java.desktop;
 
