d50cdd1be99ea6ceb196ac5da4cdc43a02eba435
federicoconsorte <federico.consorte@mail.polimi.it>
2024-05-19 12:06:06 +0200

    GUI implementation, it does not work but i need to merge with main

---
+++ src/main/java/run/ColorReadySceneController.java
@@ -0,0 +1,36 @@
+package run;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class ColorReadySceneController extends  SceneController{
+
+    public Pane loginScene;
+    public Label label_ready;
+    public Label label_title;
+    public Button red_Button;
+    public Button green_Button;
+    public Button blue_Button;
+    public Button yellow_Button;
+    public Label label_color;
+    public Button readyButton;
+
+
+    public void askColorRed(ActionEvent actionEvent) {
+    }
+
+    public void askColorGreen(ActionEvent actionEvent) {
+    }
+
+    public void askColorBlue(ActionEvent actionEvent) {
+    }
+
+    public void askColorYellow(ActionEvent actionEvent) {
+    }
+
+    public void askSetReady(ActionEvent actionEvent) {
+
+    }
+}
