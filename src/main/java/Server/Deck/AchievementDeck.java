e64da7e29a59c979f79c539032491b9481b9a828
federicoconsorte <federico.consorte@mail.polimi.it>
2024-06-21 19:53:19 +0200

    All messages except messages and controller works

--- src/main/java/Server/Deck/AchievementDeck.java
+++ src/main/java/Server/Deck/AchievementDeck.java
@@ -40,6 +40,20 @@ public class AchievementDeck implements Deckable{
         }
     }
 
+    public AchievementDeck(Boolean test){
+        this.boardCards = new HashMap<>();
+        this.cards = new ArrayList<>();
+        boardCards.put(FIRST_CARD, null);
+        boardCards.put(SECOND_CARD, null);
+        createCards();
+        try {
+            moveCardToBoard(FIRST_CARD);
+            moveCardToBoard(SECOND_CARD);
+        } catch (Exception e) {
+            System.err.println("Error while moving cards to the board (AchievementDeck)");
+        }
+    }
+
     /**
      * Shuffle the deck
      */
